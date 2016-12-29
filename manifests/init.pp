6d5efe0433e01d869a74a1ad0b4285b80d7238d0
Dennis Ploeger <dennis.ploeger@getit.de>
2016-12-29 16:33:25 +0100

    First documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,33 @@
 # Class: graylogcollectorsidecar
 # ===========================
 #
-# Full description of class graylogcollectorsidecar here.
+# Installs and configures graylog-collector-sidecar
+# (https://github.com/Graylog2/collector-sidecar)
 #
 # Parameters
 # ----------
 #
 # Document parameters here.
 #
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
+# * `version`
+# Select the version of the collector to install. Defaults to 'latest',
+# which selects the latest available release version
+# * `api_url`
+# Graylog server api url (e.g. http://graylog.example.com:9000/api)
+# * `tags`
+# An array of tags that the collector should be set up with
 #
 # Examples
 # --------
 #
 # @example
 #    class { 'graylogcollectorsidecar':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#      api_url => 'http://graylog.example.com:9000/api',
+#      tags => [ 'apache.accesslog' ]
 #    }
 #
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2016 Your name here, unless otherwise noted.
-#
 class graylogcollectorsidecar {
+
 
 
 }
